4fd28e7b5b495fb211da4fe575001e3f62188a53
Kaustubh Gadkari -X (kgadkari - ZENSAR TECHNOLOGIES INC at Cisco) <kgadkari@partnet.cisco.com>
2019-09-23 20:55:11 +0530

    Done

--- src/main/java/com/zensar/User/UserController.java
+++ src/main/java/com/zensar/User/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class UserController {
 	
